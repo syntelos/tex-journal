2206305d761502c0a01032da5852be032e44944a
John Pritchard <jdp@syntelos.com>
2016-07-31 13:34:38 -0400

    seventh

---
+++ file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+file=$(adds | grep '\.png$' | tail -n 1)
+
+if [ -n "${file}" ]&&[ -f "${file}" ]
+then
+    if [ -n "${1}" ]
+    then
+	fext="${1}"
+	base=$(basename ${file} ".png" )
+	if [ -n "$(echo "${fext}" | egrep '^\.')" ]
+	then
+	    file="${base}${fext}"
+	else
+	    file="${base}.${fext}"
+	fi
+    fi
+    echo "${file}"
+    exit 0
+else
+    cat<<EOF>&2
+$0: file not found.
+EOF
+    exit 1
+fi
