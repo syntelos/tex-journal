c9ce09fb441270e2efd5b6337faae1a624219b96
John Pritchard <jdp@syntelos.com>
2016-10-17 19:57:41 -0400

    hive mind

--- file.sh
+++ file.sh
@@ -1,22 +1,91 @@
 #!/bin/bash
+#
 
+function usage {
+    cat<<EOF>&2
+Synopsis
+
+  $0 
+
+Description
+
+  Print current file date and item for file name extension 'tex'.
+
+Synopsis
+
+  $0  [a-z][a-z][a-z]
+
+Description
+
+  Print current file date and item for file name extension '\$1'.
+
+
+Synopsis
+
+  $0 [+-]N
+
+Description
+
+  Basic arithmetic over current file item.
+
+EOF
+    return 1
+}
+
+fext=tex
+del=0
+
+#
+#
+while [ -n "${1}" ]
+do
+    case "${1}" in
+	[+-]*)
+	    exp="$del $(echo ${1} | sed 's/./& /')"
+
+	    del=$(( ${exp} ))
+
+	    del_exp="$(echo ${del} | sed 's/./& /; s/^ //; s/ $//;')"
+	    ;;
+
+	[a-z][a-z][a-z])
+	    fext="${1}"
+	    ;;
+	\*)
+	    fext="${1}"
+	    ;;
+	*)
+	    usage
+	    ;;
+    esac
+    shift
+done
+
+#
+#
 file=$(ls journal-*.{tex,txt} | tail -n 1)
 
 if [ -n "${file}" ]&&[ -f "${file}" ]
 then
-    if [ -n "${1}" ]
-    then
-	fext="${1}"
-	base=$(echo ${file} | sed 's%\.txt$%%; s%\.tex$%%;' )
 
-	if [ -n "$(echo "${fext}" | egrep '^\.')" ]
-	then
-	    file="${base}${fext}"
-	else
-	    file="${base}.${fext}"
-	fi
+    base=$(echo ${file} | sed 's%\.txt$%%; s%\.tex$%%;' )
+
+    if [ 0 -ne $del ]
+    then
+	base_date=$(echo $base | sed 's%-.$%%; ')
+
+	base_item=$(echo $base | sed "s%${base_date}%%; s%-%%;")
+
+	base_item=$(( ${base_item} ${del_exp} ))
+
+	base=${base_date}-${base_item}
     fi
+
+
+    file="${base}.${fext}"
+
     echo "${file}"
+
     exit 0
 
 else
