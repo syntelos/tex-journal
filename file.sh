018cb99520518213ec798dc7c64ef256c43c44ba
John Pritchard <jdp@syntelos.com>
2016-10-19 13:55:21 -0400

    delta arithmetic for date and item

--- file.sh
+++ file.sh
@@ -22,30 +22,48 @@ Description
 
 Synopsis
 
-  $0 [+-]N
+  $0 %[di][+-]N
 
 Description
 
-  Basic arithmetic over current file item.
+  Delta arithmetic over current file date (%d-1) or item (%i+1) for
+  single digit N.
+
 
 EOF
     return 1
 }
 
+#
+#
 fext=tex
-del=0
+del_date=0
+del_item=0
 
 #
 #
 while [ -n "${1}" ]
 do
     case "${1}" in
+
+	%d[+-][0-9])
+	    exp="0 $(echo ${1} | sed 's/%d//; s/./& /;')"
+
+	    del_date=$(( ${exp} ))
+
+	    del_date="$(echo ${del_date} | sed 's/./& /; s/^ //; s/ $//; s/^[0-9]/+ &/;')"
+	    ;;
+
+	%i[+-][0-9])
+	    exp="0 $(echo ${1} | sed 's/%i//; s/./& /;')"
+
+	    del_item=$(( ${exp} ))
+
+	    del_item="$(echo ${del_item} | sed 's/./& /; s/^ //; s/ $//; s/^[0-9]/+ &/;')"
+	    ;;
+
 	[+-]*)
-	    exp="$del $(echo ${1} | sed 's/./& /')"
-
-	    del=$(( ${exp} ))
-
-	    del_exp="$(echo ${del} | sed 's/./& /; s/^ //; s/ $//;')"
+	    usage 
 	    ;;
 
 	[a-z][a-z][a-z])
@@ -68,17 +86,27 @@ file=$(ls journal-*.{tex,txt} | tail -n 1)
 if [ -n "${file}" ]&&[ -f "${file}" ]
 then
 
-    base=$(echo ${file} | sed 's%\.txt$%%; s%\.tex$%%;' )
+    digits=$(echo ${file} | sed 's%journal-%%; s%\.txt$%%; s%\.tex$%%;' )
 
-    if [ 0 -ne $del ]
+    if [ "0" != "${del_date}" ] || [ "0" != "${del_item}" ]
     then
-	base_date=$(echo $base | sed 's%-.$%%; ')
+	base_date=$(echo "${digits}" | sed 's%-.$%%;')
 
-	base_item=$(echo $base | sed "s%${base_date}%%; s%-%%;")
+	base_item=$(echo "${digits}" | sed "s%${base_date}%%; s%-%%;")
 
-	base_item=$(( ${base_item} ${del_exp} ))
+	if [ "0" != "${del_item}" ]
+	then
+	    base_item=$(( ${base_item} ${del_item} ))
+	fi
 
-	base=${base_date}-${base_item}
+	if [ "0" != "${del_date}" ]
+	then
+	    base_date=$(( ${base_date} ${del_date} ))
+	fi
+
+	base="journal-${base_date}-${base_item}"
+    else
+	base="journal-${digits}"
     fi
 
 
