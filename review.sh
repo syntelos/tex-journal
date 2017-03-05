21c42f9628a87fe8f00b91f76ff86c5c29820161
John Pritchard <jdp@syntelos.com>
2017-03-04 20:53:42 -0500

    generalize to notes

--- review.sh
+++ review.sh
@@ -24,27 +24,40 @@ case "${1}" in
 [0-9]*)
   re="${1}"
   ;;
+[a-z-]*)
+  re="${1}"
+  ;;
 *)
   usage
   ;;
 esac
 
-for src in journal-*${re}*.tex 
-do
-  less ${src}
-  read -p "delete? [yN] " -n 1 r_del
-  case "${r_del}" in
-  [yY])
-    echo "D ${src}" >> ${log}
-    echo # (read -n 1)
-    ;;
-  [nN])
-    echo "X ${src}" >> ${log}
-    echo # (read -n 1)
-    ;;
-  *)
-    echo "X ${src}" >> ${log}
-    ;;
-  esac
 
-done
+if flist=$(2>/dev/null ls *${re}*.tex ) && [ -n "${flist}" ]
+then
+ for src in ${flist}
+ do
+   less ${src}
+   read -p "delete? [yN] " -n 1 r_del
+   case "${r_del}" in
+   [yY])
+     echo "D ${src}" >> ${log}
+     echo # (read -n 1)
+     ;;
+   [nN])
+     echo "X ${src}" >> ${log}
+     echo # (read -n 1)
+     ;;
+   *)
+     echo "X ${src}" >> ${log}
+     ;;
+   esac
+
+ done
+ exit 0
+else
+ cat<<EOF>&2
+$0 error, empty list of files from 'ls *${re}*.tex'.
+EOF
+ exit 1
+fi
