388d3597997b08656d0bec0ec12baecb3e36650f
John Pritchard <jdp@syntelos.com>
2017-03-22 10:57:28 -0400

    copt output

--- print.sh
+++ print.sh
@@ -4,14 +4,6 @@ gen_ps=false
 gen_pdf=false
 gen_png=true
 
-src=$(./file.sh $* tex )
-
-name=$(basename ${src} .tex)
-
-tgt_png=${name}.png
-tgt_pdf=${name}.pdf
-tgt_ps=${name}.ps
-tgt_dvi=${name}.dvi
 
 #
 function usage {
@@ -20,12 +12,14 @@ function usage {
   
 Synopsis
 
-  ${0} (optional file.sh args)
+  ${0} [[+-]{ps,pdf,png}] (optional file.sh args)
 
 Description
 
   Overwrite the last or optionally referenced target.  Reports 'U' for
   write, and 'X' for error.
+
+  Optionally generate ps or ps and pdf instead of the default png.
 
 EOF
 
@@ -108,6 +102,62 @@ EOF
 	return 1
     fi
 }
+
+#
+while [ -n "${1}" ]
+do
+    arg="${1}"
+    case "${arg}" in
+	+ps)
+	    gen_ps=true
+	    gen_pdf=false
+	    gen_png=false
+	    shift
+	    ;;
+	+pdf)
+	    gen_ps=true
+	    gen_pdf=true
+	    gen_png=false
+	    shift
+	    ;;
+	+png)
+	    gen_png=true
+	    shift
+	    ;;
+	-ps)
+	    gen_ps=false
+	    gen_pdf=false
+	    gen_png=true
+	    shift
+	    ;;
+	-pdf)
+	    gen_pdf=false
+	    shift
+	    ;;
+	-png)
+	    gen_png=false
+	    shift
+	    ;;
+	-h|-\?|--help)
+	    usage
+	    exit 1
+	    ;;
+	*)
+	    break
+	    ;;
+    esac
+
+done
+
+#
+src=$(./file.sh $* tex )
+
+name=$(basename ${src} .tex)
+
+tgt_png=${name}.png
+tgt_pdf=${name}.pdf
+tgt_ps=${name}.ps
+tgt_dvi=${name}.dvi
 
 #
 if compile
